931a8c5795f626f9dd0b7ba2b8747b1fbc6d9cce
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-13 14:38:16 +0900

    chore: validate macaddress

--- src/wol.rs
+++ src/wol.rs
@@ -6,16 +6,17 @@ use hex::*;
 use regex::Regex;
 use wake_on_lan::*;
 
-fn validate_mac_address(str_mac: &str) -> bool {
-    let set = Regex::new(r"^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$").unwrap();
-    return Regex::is_match(&set, str_mac);
+fn validate_mac_address(mac_address: &str) -> bool {
+    Regex::new(r"^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$")
+        .unwrap()
+        .is_match(mac_address)
 }
 
-pub fn convert_mac_address(str_mac: &str) -> Result<[u8; 6], ()> {
-    if !validate_mac_address(str_mac) {
+pub fn convert_mac_address(mac_address: &str) -> Result<[u8; 6], ()> {
+    if !validate_mac_address(mac_address) {
         return Err(());
     }
-    let macaddress = decode(str_mac.replace(":", "")).unwrap();
+    let macaddress = decode(mac_address.replace(":", "")).unwrap();
 
     Ok(macaddress.try_into().unwrap())
 }
