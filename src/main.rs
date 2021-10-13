b436f6657170ba946c4bc2d244835941f935b8b7
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-13 14:33:44 +0900

    fix: クソコード修正

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-// extern crate reqwest;
+extern crate reqwest;
 extern crate wake_on_lan;
 
 // pub mod slack;
 pub mod wol;
 
 fn main() {
-    let mac: &str="18:C0:4D:94:06:7F";
-    let validity=wol::mac_validate(mac);
-    println!("Mac Validity : {}",validity);
-    if wol::mac_validate(mac){
-        let mac_byte=wol::convert_mac_address(mac);
-        println!("MacAddress : {:?}",mac_byte);
+    let mac: &str = "18:C0:4D:94:06:7F";
+
+    match wol::convert_mac_address(mac) {
+        Ok(mac_address) => {
+            println!("Mac address : {:?}", mac_address);
+        }
+        Err(_) => {
+            println!("Invalid mac address.");
+        }
     }
-    
 }
--- src/wol.rs
+++ src/wol.rs
@@ -1,18 +1,23 @@
 extern crate hex;
 
+use std::convert::TryInto;
+
 use hex::*;
 use regex::Regex;
 use wake_on_lan::*;
 
-pub fn mac_validate(str_mac:&str)->bool{
+fn validate_mac_address(str_mac: &str) -> bool {
     let set = Regex::new(r"^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$").unwrap();
     return Regex::is_match(&set, str_mac);
 }
 
-pub fn convert_mac_address(str_mac:&str)->[u8;6]{
-    let macaddress_vec: Vec<_>=decode(str_mac.replace(":","")).unwrap();
-    let macaddress:[u8;6]=[macaddress_vec[0],macaddress_vec[1],macaddress_vec[2],macaddress_vec[3],macaddress_vec[4],macaddress_vec[5]];
-    return macaddress;
+pub fn convert_mac_address(str_mac: &str) -> Result<[u8; 6], ()> {
+    if !validate_mac_address(str_mac) {
+        return Err(());
+    }
+    let macaddress = decode(str_mac.replace(":", "")).unwrap();
+
+    Ok(macaddress.try_into().unwrap())
 }
 
 pub fn wake_on_lan(mac_address: [u8; 6]) -> Result<(), std::io::Error> {
