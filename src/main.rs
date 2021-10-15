d771361869800f5808a39b133daade0e292e27d4
Ryo <63697155+rq5273@users.noreply.github.com>
2021-10-15 19:15:05 +0900

    websocket challenge

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
+extern crate tokio;
+extern crate serde_json;
 extern crate reqwest;
 extern crate wake_on_lan;
+extern crate websocket;
 
-// pub mod slack;
+
+use std::fs::File;
+use std::io::BufReader;
+
+
+
+pub mod slack;
 pub mod wol;
+pub mod command;
 
-fn main() {
-    let mac: &str = "18:C0:4D:94:06:7F";
 
-    match wol::convert_mac_address(mac) {
-        Ok(mac_address) => {
-            println!("Mac address : {:?}", mac_address);
-        }
-        Err(_) => {
-            println!("Invalid mac address.");
-        }
+
+#[tokio::main]
+async fn main() {
+    let file = File::open("/home/rq5273/.smart-home-slack/token.json").unwrap();
+    let reader = BufReader::new(file);
+    let token_json:serde_json::Value = serde_json::from_reader(reader).unwrap();
+    let slack_token=token_json["xapp"].as_str().unwrap();
+    let ws_url=slack::get_websocket_url(slack_token.to_string()).await;
+    
+    if ws_url["ok"].as_bool().unwrap(){
+        println!("url: {}",ws_url["url"].as_str().unwrap());
+        slack::start2(ws_url["url"].as_str().unwrap().to_string()+"&debug_reconnects=true");
+        // let msg=slack::start2(ws_url["url"].as_str().unwrap().to_string()+"&debug_reconnects=true");
+        // println!("msg:{:?}",msg);
     }
+    
+
+
+
+    // let mac: &str = "18:C0:4D:94:06:7F";
+
+    // match wol::convert_mac_address(mac) {
+    //     Ok(mac_address) => {
+    //         println!("Mac address : {:?}", mac_address);
+    //     }
+    //     Err(_) => {
+    //         println!("Invalid mac address.");
+    //     }
+    // }
 }
--- src/slack.rs
+++ src/slack.rs
@@ -1,6 +1,7 @@
 use ws::{connect, CloseCode};
+use websocket::{ClientBuilder, OwnedMessage};
 
-pub async fn get_websocket_url(slack_token: String) -> Result<String, reqwest::Error> {
+pub async fn get_websocket_url(slack_token: String) -> serde_json::Value {
     let client = reqwest::Client::new();
     let response = client
         .post("https://slack.com/api/apps.connections.open")
@@ -9,19 +10,29 @@ pub async fn get_websocket_url(slack_token: String) -> Result<String, reqwest::E
             format!("Bearer {}", slack_token),
         )
         .send()
-        .await?;
+        .await.unwrap();
 
-    todo!()
+    let responsetext= response.text().await.unwrap();
+    let responsejson:serde_json::Value=serde_json::from_str(&responsetext).unwrap();
+    return responsejson
 }
 
 pub async fn start(websocket_url: String) {
     connect(websocket_url, |out| {
         out.send("Hello WebSocket").unwrap();
-
+        
         move |msg| {
+            println!("receive");
             println!("Got message: {}", msg);
             out.close(CloseCode::Normal)
         }
     })
     .unwrap()
+}
+
+pub fn start2(websocket_url: String)->OwnedMessage{
+    let client = ClientBuilder::new(&websocket_url).unwrap().connect_insecure().unwrap();
+    let (mut receiver, sender) = client.split().unwrap();
+    let msg=receiver.recv_message().unwrap();
+    return msg
 }
